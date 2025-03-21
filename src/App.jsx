2a321f13c75f25173ba3eb72c33852c50ba373cb
Marco Mechini <mechinimarco35@gmail.com>
2025-03-21 11:04:22 +0100

    bonus use Reduce e style

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useReducer, useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   const products = [
     { name: 'Mela', price: 0.5 },
@@ -11,80 +10,77 @@ function App() {
     { name: 'Pasta', price: 0.7 },
   ];
 
-  // const [cartProducts, dispatchCartProducts] = useReducer(cartReducer, products)
-  const [addedProducts, setAddedProducts] = useState([])
+  const [cartProducts, dispatchCartProducts] = useReducer(cartReducer, [])
 
   function cartReducer(state, action) {
+    console.log('state', state);
+    console.log('action', action);
+
     switch (action.type) {
       case 'ADD_ITEM':
         // Logica per aggiungere un prodotto
+        if (!state.find(p => p.name === action.prodotto.name)) {
+          return ([...state, { name: action.prodotto.name, price: action.prodotto.price, quantity: 1 }])
+        }
+        else {
+          dispatchCartProducts({ type: 'UPDATE_QUANTITY', index: action.index, numberToAdd: 1 })
+        }
         break;
       case 'REMOVE_ITEM':
         // Logica per rimuovere un prodotto
-        break;
+        return state.filter((p, index) => action.i !== index)
       case 'UPDATE_QUANTITY':
         // Logica per aggiornare la quantità
-        break;
+        return (state.map((prod, i) =>
+          (action.numberToAdd === 1) ?
+            (i === action.index) ? { ...prod, quantity: prod.quantity + action.numberToAdd } : prod
+            : (i === action.index) ? { ...prod, quantity: prod.quantity = action.numberToAdd } : prod
+
+        ))
       default:
         return state;
     }
   }
 
-  const removeFromCart = (i) => {
-    // dispatchCartProducts({type: 'REMOVE_ITEM', index: i})
-    const filtered = addedProducts.filter((p, index) => i !== index)
-    setAddedProducts(filtered)
-  }
-
-  const updateProductQuantity = (index, numberToAdd = 1) => {
-    setAddedProducts(prevProd =>
-      prevProd.map((prod, i) =>
-        (numberToAdd === 1) ?
-          (i === index) ? { ...prod, quantity: prod.quantity + numberToAdd } : prod
-          : (i === index) ? { ...prod, quantity: prod.quantity = numberToAdd } : prod
-
-      ))
-  }
-
-
-  const addToCart = (prodotto, index) => {
-    if (!addedProducts.find(p => p.name === prodotto.name)) {
-      setAddedProducts([...addedProducts, { name: prodotto.name, price: prodotto.price, quantity: 1 }])
-    } else {
-      // salvo in addedProducts un nuovo array modificato con map dove ho trovato corrispondenza tra gli indici faccio quantità + 1 sennò ritorno solo la quantità
-      updateProductQuantity(index);
-    }
-  }
-
   return (
     <>
-      <ul>
+      <section className='product-list'>
         {products.map((p, i) => {
           return (
-            <li key={i}>
-              <p>{p.name}</p>
-              <p>{p.price}</p>
-              <button onClick={() => addToCart(p, i)}>Aggiungi al carrello</button>
-            </li>)
+            <div key={i}>
+              <p>{p.name} ({p.price.toFixed(2)}$)</p>
+              <button
+                onClick={() => dispatchCartProducts({ type: 'ADD_ITEM', prodotto: p, index: i })}>
+                Aggiungi al carrello
+              </button>
+            </div>)
         })}
-      </ul>
-      <ul>
-        {addedProducts && addedProducts.map((p, i) => {
+      </section>
+      <section className='product-cart'>
+        {cartProducts && cartProducts.map((p, i) => {
           return (
-            <li key={i}>
+            <div key={i}>
               <p>Nome: {p.name}</p>
-              <p>Prezzo: {p.price}$</p>
+              <p>Prezzo: {p.price.toFixed(2)}$</p>
               <div>
                 <label htmlFor="quantity">Inseritisci quantità</label>
-                <input type="number" name="quantity" id='quantity' value={p.quantity} onChange={(e) => updateProductQuantity(i, e.target.value)} />
+                <input
+                  type="number"
+                  name="quantity"
+                  id='quantity'
+                  value={p.quantity}
+                  onChange={(e) => dispatchCartProducts({ type: 'UPDATE_QUANTITY', index: i, numberToAdd: e.target.value })} />
               </div>
-              <button onClick={() => removeFromCart(i)}>Rimuovi dal carrello</button>
-            </li>)
+              <button
+                onClick={() => dispatchCartProducts({ type: 'REMOVE_ITEM', i })}>
+                Rimuovi dal carrello
+              </button>
+            </div>)
         })}
-      </ul>
+      </section>
       <h4>Totale</h4>
       <div>
-        {addedProducts && addedProducts.reduce((acc, product) => {
+        {cartProducts && cartProducts.reduce((acc, product) => {
           return acc + product.price * product.quantity;
         }, 0)} $
       </div>
